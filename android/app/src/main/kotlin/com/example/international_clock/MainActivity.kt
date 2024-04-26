23cf073ccc43d956c9e0f1debf68578e13ff9668
Jose Laurito <jigolaurito93@yahoo.com>
2024-04-26 00:20:42 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/international_clock/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.international_clock
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:international_clock/pages/home.dart';
+import 'package:international_clock/pages/choose_location.dart';
+import 'package:international_clock/pages/loading.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Home(),
+    ),
+  );
+}

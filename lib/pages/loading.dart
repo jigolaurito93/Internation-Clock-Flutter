36b47eec6d27229e10a9e9f87b13bcb67e2193d0
Jose Laurito <jigolaurito93@yahoo.com>
2024-04-27 03:38:42 -0500

    installed package with loaders and spinners for loading page

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:international_clock/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -23,16 +24,20 @@ class _LoadingState extends State<Loading> {
   @override
   void initState() {
     super.initState();
-    setupWorldTime();
+    Future.delayed(const Duration(seconds: 3), () {
+      setupWorldTime();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50),
-        child: Text('loading'),
-      ),
-    );
+    return Scaffold(
+        backgroundColor: Colors.blue[900],
+        body: const Center(
+          child: SpinKitWanderingCubes(
+            color: Colors.white,
+            size: 80.0,
+          ),
+        ));
   }
 }
